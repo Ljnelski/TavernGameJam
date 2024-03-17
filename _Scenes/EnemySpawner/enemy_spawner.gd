e7b8c760d27bd19a5047d5c8fd19036583bb9325
Ljnelski <Ljnelski@gmail.com>
2024-03-17 13:25:44 -0400

    Continued work on the enemy spawner

--- _Scenes/EnemySpawner/enemy_spawner.gd
+++ _Scenes/EnemySpawner/enemy_spawner.gd
@@ -1,11 +1,44 @@
 extends Node3D
 
+@export var world_width : float;
+@export var spawn_count: float;
+@export var spawn_padding: float;
+
+@export var enemy : PackedScene;
+@export var spawn_rate: float = 2.0;
+
+
+var spawn_locations : Array[Node3D];
+var cool_down : float;
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# create spawn locations.
+	spawn_locations = [];
+	
+	var start_pos = position.x - (world_width/2) + spawn_padding;
+	var end_pos = position.x + (world_width/2) - spawn_padding;
+	
+	var distance = abs(end_pos - start_pos) / spawn_count;
+	print("world start: %d" % start_pos + ", world end: %d" % end_pos);
 
+	
+	for i in spawn_count:
+		print("creating spawn");
+		var spawn_pos : Vector3 = Vector3(start_pos + (0.5 * distance + i * distance) , position.y, position.z);
+		var spawn = Node3D.new()
+		add_child(spawn);
+		spawn.set_position(spawn_pos);
+		spawn_locations.append(spawn);	
+	
+	print(spawn_locations.size());
+	
+	for n in spawn_locations:
+		print(n.position);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if cool_down <= 0:
+		enemy.instantiate()
 	pass
